32d7b0f27eea501c6e116ed52a590eb18778bd61
Mukul Verma <mukulv072@gmail.com>
2026-02-24 16:04:17 +0530

    added more pages

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ import path from 'path'
 
 const app = express();
 
-let absPath = path.resolve("index.html")
-
-app.use(express.static("style.css"))
+let absPath = path.resolve("pages")
+app.use(express.static("styles"))
 
 app.get("/", (req, res) => {
-    res.sendFile(absPath)
+    res.sendFile(absPath + "/index.html")
 })
 
 app.get("/about", (req, res) => {
     res.send("<h1>about page</h1>");
 })
-app.get("/completed", (req, res) => {
-    res.send("<h1>completed page</h1>");
+app.get("/login", (req, res) => {
+    res.send("<h1>login page</h1>");
+})
+app.get("/signup", (req, res) => {
+    res.send("<h1>signup page</h1>");
 })
 app.get("/todo", (req, res) => {
     res.send("<h1>todo page</h1>");
 })
 
+app.use((req, res) => {
+    res.status(404).sendFile(absPath + "/error.html")
+})
 app.listen(4500);
