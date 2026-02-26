4ed0937b7d83f2624dd0401a1e7c2a5ee424c066
Mukul Verma <mukulv072@gmail.com>
2026-02-26 11:21:01 +0530

    Made Routes Secure

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 import express from 'express'
-import qs from 'querystring'
 import fs from 'fs'
 
 const app = express();
+
+let loginToken = false;
+function route(req, res, next) {
+    if (!loginToken) {
+        res.send("<h1>Your are not allowed! Please login First</h1>");
+    }
+    else {
+        next();
+    }
+}
 
 app.set("view engine", "ejs");
 
 // Static CSS //
 app.use(express.static("styles"));
+
+// Get Form data in Simple Way //
+app.use(express.urlencoded({ extended: false }))
 
 // Home Page //
 app.get("/", (req, res) => {
@@ -19,7 +31,6 @@ app.get("/about", (req, res) => {
     res.render("about");
 })
 
-
 // Sign up Page //
 app.get("/signup", (req, res) => {
     res.render("signup");
@@ -27,26 +38,19 @@ app.get("/signup", (req, res) => {
 
 // Sign up Logic
 app.post("/signup", (req, res) => {
-    let body = ''
-    req.on("data", (chunk) => {
-        body += chunk;
-    })
+    const data = req.body;
 
-    req.on("end", () => {
-        let data = qs.parse(body);
-        console.log(data);
-        fs.readFile("database/users.json", "utf-8", (err, userData) => {
-            let users = [];
-            if (!err && userData) {
-                users = JSON.parse(userData);
+    fs.readFile("database/users.json", "utf-8", (err, userData) => {
+        let users = [];
+        if (!err && userData) {
+            users = JSON.parse(userData);
+        }
+        users.push(data);
+        fs.writeFile("database/users.json", JSON.stringify(users, null, 2), (err) => {
+            if (err) {
+                console.log("Error occured", err)
             }
-            users.push(data);
-            fs.writeFile("database/users.json", JSON.stringify(users, null, 2), (err) => {
-                if (err) {
-                    console.log("Error occured", err)
-                }
-                res.redirect("/login")
-            })
+            res.redirect("/login")
         })
     })
 })
@@ -58,61 +62,51 @@ app.get("/login", (req, res) => {
 
 // Login Logic //
 app.post("/login", (req, res) => {
-    let body = ""
-    req.on("data", (chunk) => {
-        body += chunk;
-    })
+    const data = req.body;
 
-    req.on("end", () => {
-        let data = qs.parse(body);
-        fs.readFile("database/users.json", "utf-8", (err, userData) => {
-            if (err) {
-                console.log("Error occured", err);
-            }
-            let users = []
-            users = JSON.parse(userData);
-            const user = users.find(u => u.email === data.email);
+    fs.readFile("database/users.json", "utf-8", (err, userData) => {
+        if (err) {
+            console.log("Error occured", err);
+        }
+        let users = []
+        users = JSON.parse(userData);
+        const user = users.find(u => u.email === data.email);
 
-            if (!user) {
-                console.log("User not found");
-                return res.send("<h1>User not found</h1>")
+        if (!user) {
+            console.log("User not found");
+            res.send("<h1>User not found</h1>")
+        }
+        else {
+            if (user.password === data.password) {
+                loginToken = true;
+                res.redirect("/")
             }
             else {
-                if (user.password === data.password) {
-                    res.redirect("/")
-                }
-                else {
-                    console.log("Wrong password");
-                }
+                res.redirect('/login')
+                console.log("Wrong password");
             }
-        })
+        }
     })
 })
 
 // todo Page //
-app.get("/todo", (req, res) => {
+app.get("/todo", route, (req, res) => {
     res.render("todo");
 })
 
 // todo Logic //
 app.post("/addTask", (req, res) => {
-    let body = ""
-    req.on("data", (chunk) => {
-        body += chunk;
-    })
+    const data = req.body;
 
-    req.on("end", () => {
-        let data = qs.parse(body)
-        fs.readFile("database/tasks.json", "utf-8", (err, taskData) => {
-            let tasks = []
-            if (!err && taskData)
-                tasks = JSON.parse(taskData)
-            tasks.push(data);
-            fs.writeFile("database/tasks.json", JSON.stringify(tasks, null, 2), (err) => {
-                if (err) return res.send("<h1>Error Occured</h1>")
-                else
-                    res.redirect("/");
-            })
+    fs.readFile("database/tasks.json", "utf-8", (err, taskData) => {
+        let tasks = []
+        if (!err && taskData)
+            tasks = JSON.parse(taskData)
+        tasks.push(data);
+        fs.writeFile("database/tasks.json", JSON.stringify(tasks, null, 2), (err) => {
+            if (err) return res.send("<h1>Error Occured</h1>")
+            else
+                res.send(data.todo);
         })
     })
 })
